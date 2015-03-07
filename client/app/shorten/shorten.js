2708e74baf1144611f7fb11102188a4c4644e4eb
KalevRK <KalevRK@gmail.com>
2015-03-06 17:02:19 -0800

    App shortens and displays urls

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,8 +2,14 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $newUrl = '';
   $scope.link = {};
+  $scope.validate = function(str){
+
+    return JSON.stringify({url: str});
+  };
   $scope.addLink = function(url){
+
     Links.addLink(url);
   };
 });
